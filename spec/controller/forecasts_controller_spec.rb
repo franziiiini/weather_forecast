814561e6bcd78efd8eca93ad6a5207144b42640c
franziiiini <franzi.nicholson@gmail.com>
2025-03-15 14:49:09 -0400

    Update tests

--- spec/controller/forecasts_controller_spec.rb
+++ spec/controller/forecasts_controller_spec.rb
@@ -2,25 +2,24 @@ require "rails_helper"
 
 RSpec.describe ForecastsController, type: :controller do
   describe "GET #index" do
-    it "renders the index template & returns 200" do
-      get :index
+    context "when no params are present" do
+      it "returns 200" do
+        get :index
 
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'accepts input params and returns the weather api response' do
-      weather_api_client = double(WeatherApiClient)
-      allow(weather_api_client).to receive(:parsed_response).and_return(parsed_forecast_json)
+    context 'with location params' do
+      it 'accepts params calls the weather api client' do
+        weather_api_client = double
+        allow(WeatherApiClient).to receive(:new).with("10709").and_return(weather_api_client)
+        allow(weather_api_client).to receive(:mapped_weather_attributes)
 
-      get :index, params: { location: "10709" }
-    end
+        get :index, params: { location: "10709" }
 
-    private
-
-    def parsed_forecast_json
-      JSON.parse(
-        File.read("spec/fixtures/forecast.json")
-      )
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
--- spec/features/user_sees_weather_information_spec.rb
+++ spec/features/user_sees_weather_information_spec.rb
@@ -1,14 +1,36 @@
 require "rails_helper"
 
-RSpec.describe "user sees weather information", type: :feature do
-  context "with city name as an input" do
-    it "show the current location" do
+RSpec.describe "user searches for weather information", type: :feature do
+  context "when user submits a valid input" do
+    it "shows the current location and weather information" do
+      weather_api_client = double
+      allow(WeatherApiClient).to receive(:new).with("Eastchester").and_return(weather_api_client)
+      allow(weather_api_client).to receive(:mapped_weather_attributes).and_return(
+        {
+          location_name: "Eastchester, NY, USA",
+          current_temp: "50",
+          condition: "Cloudy",
+          min_temp: "40",
+          max_temp: "50"
+        }
+      )
+
       visit "/"
 
       fill_in("location", with: "Eastchester")
       click_button("Submit")
 
-      expect(page).to have_content("The current temperature in Eastchester, NY, USA, is")
+      expect(page).to have_content(
+        "The current temperature in Eastchester, NY, USA is 50 F."
+        )
+      expect(page).to have_content(
+        "Expect cloudy conditions, with a low of 40 F and a high of 50 F."
+        )
+    end
+  end
+
+  context 'when user submits an invalid input' do
+    it 'shows an error that the location was not found' do
     end
   end
 end
